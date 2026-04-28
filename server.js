92c8cbd84684cdd4f76b6e2846d524af2e9a6446
saikeoscorp-lgtm <saikeoscorp@gmail.com>
2026-04-29 00:36:04 +0300

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const { error } = require("console");
 const app = express();
 const PORT =  process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Server running on port ${PORT}`);
 });
 
 app.use(express.json());
