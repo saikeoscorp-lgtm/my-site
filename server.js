ac9ac767fdb2b99c2f0b5a4bc76ff2cdfd310f0b
saikeoscorp-lgtm <saikeoscorp@gmail.com>
2026-04-29 11:37:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
