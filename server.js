feac30a1b6c2127376da3aa90249031508f3ada3
saikeoscorp-lgtm <saikeoscorp@gmail.com>
2026-04-29 22:00:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,6 @@ const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mailer = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
-  },
-  tls: {
-  rejectUnautorized: false
-  }
-});
-
 function makeCode() {
   return String(Math.floor(100000 + Math.random() * 900000));
 }
