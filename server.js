114c216d45d3a5bfd94fc331b9ddc058c0bf4ba6
saikeoscorp-lgtm <saikeoscorp@gmail.com>
2026-05-07 17:36:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -95,13 +95,12 @@ app.use(session({
     secure: false
   }
 }));
+
 app.use("/private", (req, res, next) => {
   if (!req.session.user) {
     return res.redirect("/login.html");
   }
-  if (req.session.user.role !== "admin") {
-    return res.status(403).send("Not asecss");
-  }
+
   next();
 });
 
